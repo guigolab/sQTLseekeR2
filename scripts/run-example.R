a5f8a763a4737935722e9d41568ea864aa28e589
dgarrimar <dgarrimar@gmail.com>
2017-03-24 20:13:18 +0100

    run-example back to original

--- scripts/run-example.R
+++ scripts/run-example.R
@@ -9,29 +9,29 @@
 ## to check is the package is correctly installed and the inputs correctly formatted.
 ##
 
-#library(sQTLseekeR)
+library(sQTLseekeR2)
 
 ## Input files: transcript expression, gene location and genotype information
-trans.exp.f = "../Data/transExpression.tsv.gz"
-gene.bed.f = "../Data/genes.bed"
-genotype.f = "../Data/snps-012coded.tsv"
+trans.exp.f <- "../Data/transExpression.tsv.gz"
+gene.bed.f <- "../Data/genes.bed"
+genotype.f <- "../Data/snps-012coded.tsv"
 
 ## 1) Index the genotype file (if not done externally before)
-genotype.indexed.f = sQTLseekeR2::index.genotype(genotype.f)
+genotype.indexed.f <- index.genotype(genotype.f)
 
 ## 2) Prepare transcript expression
-te.df = read.table(trans.exp.f, as.is=TRUE, header=TRUE, sep="\t")
-tre.df = prepare.trans.exp(te.df)
+te.df <- read.table(trans.exp.f, as.is=TRUE, header=TRUE, sep="\t")
+tre.df <- prepare.trans.exp(te.df)
 
 ## 3) Test gene/SNP associations
-gene.bed = read.table(gene.bed.f, as.is=TRUE, sep="\t")
-colnames(gene.bed) = c("chr","start","end","geneId")
-res.df = sqtl.seeker(tre.df, genotype.indexed.f, gene.bed, svQTL=TRUE)
+gene.bed <- read.table(gene.bed.f, as.is=TRUE, sep="\t")
+colnames(gene.bed) <- c("chr","start","end","geneId")
+res.df <- sqtl.seeker(tre.df, genotype.indexed.f, gene.bed, svQTL = TRUE)
 
 ## Optional: write a file with all Pvalues
-res.f = "sQTLs-all.tsv"
-write.table(res.df, file=res.f, quote=FALSE, row.names=FALSE, sep="\t")
+res.f <- "sQTLs-all.tsv"
+write.table(res.df, file = res.f, quote = FALSE, row.names = FALSE, sep = "\t")
 ## Optional
 
 ## 4) Get significant sQTLs
-sqtls.df = sqtls(res.df, FDR=.01, out.pdf="sQTLs-FDR01.pdf")
+sqtls.df <- sqtls(res.df, FDR = .01, out.pdf = "sQTLs-FDR01.pdf")
