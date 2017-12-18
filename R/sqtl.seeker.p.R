f008cbaf4332cc642fb526491fefd1e40526ead8
dgarrimar <dgarrimar@gmail.com>
2017-12-18 19:29:00 +0100

    sqtl.seeker.p add covariates, new test, update doc

--- R/sqtl.seeker.p.R
+++ R/sqtl.seeker.p.R
@@ -10,16 +10,20 @@
 ##' Must have column 'snpId'. Same as in \code{sqtl.seeker}.
 ##' @param gene.loc a data.frame with the genes location. Columns 'chr', 'start',
 ##' 'end' and 'geneId' are required. Same as in \code{sqtl.seeker}.
+##' @param covariates a data.frame with covariate information per sample (samples x covariates).
+##' Rownames should be the sample ids. Covariates can be either \code{numeric} or \code{factor}. 
+##' When provided, they are regressed out before testing the genotype effect. Default is \code{NULL}.
 ##' @param genic.window the window(bp) around the gene in which the SNPs are tested. Default is 5000 (i.e. 5kb).
 ##' Same as in \code{sqtl.seeker}.
 ##' @param min.nb.ext.scores the minimum number of permuted  nominal P-values lower than
 ##' the lowest observed nominal P-value to allow the computation to stop. Default is 100. 
+##' @param nb.perm.min the minimum number of permutations. Default is 100.
 ##' @param nb.perm.max the maximum number of permutations. Default is 1000. 
 ##' @param min.nb.ind.geno SNPs with less samples than \code{min.nb.ind.geno} in any genotype group
 ##' are filtered out. Default is 10.
-##' @param verbose Default is TRUE. Mainly for debugging.
-##' @return a data.frame with columns:
-##' \item{geneId}{the gene name}
+##' @param verbose Default is TRUE. 
+##' @return A data.frame with columns:
+##' \item{geneId}{the gene name.}
 ##' \item{variants.cis}{the number of variants tested in cis.}
 ##' \item{LD}{a linkage disequilibrium estimate for the genomic window (median r2).}
 ##' \item{best.snp}{ID of the SNP with the smallest observed nominal P-value.}
@@ -33,7 +37,7 @@
 ##' @author Diego Garrido-Martín
 ##' @export
 ##' @import fitdistrplus
-sqtl.seeker.p <- function(tre.df, genotype.f, gene.loc, genic.window = 5000, min.nb.ext.scores = 100, nb.perm.max = 1000, min.nb.ind.geno = 10, verbose = FALSE){
+sqtl.seeker.p <- function(tre.df, genotype.f, gene.loc, covariates = NULL, genic.window = 5000, nb.perm.min = 100, nb.perm.max = 1000, min.nb.ext.scores = 100, min.nb.ind.geno = 10, verbose = FALSE){
   
   . <- nb.groups <- snpId <- NULL ## Uglily appease R checks (dplyr)
   
@@ -48,15 +52,45 @@ sqtl.seeker.p <- function(tre.df, genotype.f, gene.loc, genic.window = 5000, min
       gr.gene <- GenomicRanges::resize(gr.gene, GenomicRanges::width(gr.gene) + 2 * genic.window, fix = "center")
     }
     if(length(gr.gene) > 0){
+      if(!is.null(covariates)){
+        if(!all(rownames(covariates) %in% colnames(tre.gene))){
+          stop("All samples should have covariate information (either a value or NA).")                  
+        }
+        cov.na <- apply(covariates, 1, function(x){any(is.na(x))})
+        covariates <- covariates[!cov.na, ]                                     
+        if(sum(cov.na) > 0){
+          warning(sprintf("%s samples with NA values for at least one covariate have been removed.", sum(cov.na)))     
+        }
+        if(sum(cov.na) > round(0.05*nrow(covariates))){
+          stop("More than 5% of the samples contain NA values for at least one covariate")  
+        }
+      }
       ## Remove samples with non expressed genes
       tre.gene <- tre.gene[, !is.na(tre.gene[1, ])]
       ## Focus on common samples
       genotype.headers <- as.character(utils::read.table(genotype.f, as.is = TRUE, nrows = 1))
-      com.samples <- intersect(colnames(tre.gene), genotype.headers)
-      if(length(com.samples) == 0){
-        stop("No common samples between genotype and transcript ratios files.")
+      if(!is.null(covariates)){
+        com.samples <- Reduce(intersect, list(colnames(tre.gene), genotype.headers, rownames(covariates)))   
+        if(length(com.samples) == 0){
+          stop("No common samples between genotype, covariate and transcript files.")
+        }
+      } else{
+        com.samples <- intersect(colnames(tre.gene), genotype.headers)
+        if(length(com.samples) == 0){
+          stop("No common samples between genotype and transcript files.")
+        }
       }
-      tre.dist <- hellingerDist(tre.gene[, com.samples])
+      tre.gene <- tre.gene[, c("trId", "geneId", com.samples)]                     
+      tre.tc <- t(sqrt(tre.gene[,com.samples]))
+      tre.tc <- scale(tre.tc, center = T, scale = F)
+      colnames(tre.tc) <- tre.gene$tr
+      
+      # Here regress covariates and keep residual if applies
+      if(!is.null(covariates)){
+        fit <- lm(tre.tc ~ ., data = covariates)
+        tre.tc <- fit$residual
+      }
+      
       res.df <- data.frame()
       genotype.gene <- read.bedix(genotype.f, gr.gene) 
       if (verbose & is.null(genotype.gene)) {
@@ -70,10 +104,10 @@ sqtl.seeker.p <- function(tre.df, genotype.f, gene.loc, genic.window = 5000, min
         }
         if (any(snps.to.keep == "PASS")) {
           genotype.gene <- genotype.gene[snps.to.keep == "PASS", ]
-          observed <- dplyr::do(dplyr::group_by(genotype.gene, snpId), compute.nominal.pv(., tre.dist, permute = FALSE))
+          observed <- dplyr::do(dplyr::group_by(genotype.gene, snpId), compute.nominal.pv(., tre.mt = tre.tc))
           min.pv.obs <- min(observed$pv.snp) 
           best.snp <- observed$snpId[which.min(observed$pv.snp)]
-          res.df <- compute.empirical.pv(genotype.gene = genotype.gene, tre.dist = tre.dist, min.nb.ext.scores = min.nb.ext.scores,
+          res.df <- compute.empirical.pv(genotype.gene = genotype.gene, tre.mt = tre.tc, min.nb.ext.scores = min.nb.ext.scores, nb.perm.min = nb.perm.min,
                                          nb.perm.max = nb.perm.max, min.pv.obs = min.pv.obs, best.snp = best.snp, verbose = verbose)
           return(data.frame(done = TRUE, res.df))
         }
@@ -101,67 +135,75 @@ sqtl.seeker.p <- function(tre.df, genotype.f, gene.loc, genic.window = 5000, min
   }
 }
 
-##' Computes nominal P-value for the association between the genotype of a SNP and the splicing ratios of a gene.
+##' Computes nominal P-value for the association between the genotype of a locus (e.g. SNP) and the splicing ratios of a gene.
 ##' @title Compute nominal P-value
-##' @param geno.df a data.frame of a single row corresponding to the genotype of one SNP 
-##' observed in several individuals (columns).
-##' @param tre.dist a interdistance matrix produced by \code{\link{hellingerDist}}.
-##' @param permute should the interdistance matrix be permuted. Default is FALSE.
+##' @param geno.df a data.frame with a single row corresponding to the genotype of a SNP
+##' observed in several individuals.
+##' @param tre.mt a matrix of splicing ratios (samples x transcripts).
+##' @param permute should the rows of the splicing ratio matrix be permuted. Default is FALSE.
+##' @param seed if \code{permute} is TRUE, value provided to \code{\link{set.seed}}. Default is 1.
 ##' @return A data.frame containing a P-value for the association.
 ##' @author Diego Garrido-Martín 
 ##' @keywords internal
-compute.nominal.pv <- function(geno.df, tre.dist, permute = FALSE){
+compute.nominal.pv <- function(geno.df, tre.mt, permute = FALSE, seed = 1){
+  
   if (nrow(geno.df) > 1) {
     stop(geno.df$snpId[1], " SNP is duplicated in the genotype file.")
   } 
-  geno.snp <- as.numeric(geno.df[, labels(tre.dist)])
-  names(geno.snp) <- labels(tre.dist)
-  D <- as.matrix(tre.dist)
+  geno.snp <- as.numeric(geno.df[, rownames(tre.mt)])
+  names(geno.snp) <- rownames(tre.mt)
   if (any(geno.snp == -1)) {
     non.na <- geno.snp > -1
     geno.snp <- geno.snp[non.na]
-    D <- D[non.na, non.na]
+    tre.mt <- tre.mt[non.na, ]
   }
   groups.snp.f <- factor(as.numeric(geno.snp))
-  X <- stats::model.matrix(~., data = data.frame(genotype = groups.snp.f), contrasts.arg = list("genotype" = "contr.sum"))     
-  p <- ncol(X) - 1  # nb.gp - 1
-  n <- nrow(X)      # nb of non-NA individuals
-  H <- tcrossprod(tcrossprod(X, solve(crossprod(X))), X)
+  n <- nrow(tre.mt)
+
   if (permute){
+    set.seed(seed)
     perm <- sample(1:n)
-    D <- D[perm, perm]
-  }
-  G <- gower(D)
-  numer <- crossprod(c(H), c(G))
-  trG <- sum(diag(G))
-  denom <- trG - numer                                                          
-  f.snp <- as.numeric(numer/denom)                                                  
-  lambda <- eigen(G, only.values = T)$values                                    
-  lambda <- abs(lambda[abs(lambda) > 1e-12 ])                                     
-  start.acc <- 1e-14   
-  item.acc <- start.acc
-  pv.snp <- pcqf(q = f.snp, lambda = lambda, k = p, p = p, n = n, acc = start.acc)     
-  while (length(pv.snp) > 1) {                                                      
-    item.acc <- item.acc * 10
-    pv.snp <- pcqf(q = f.snp, lambda = lambda, k = p, p = p, n = n, acc = item.acc)  
-  }
-  if(pv.snp < item.acc) {
-    pv.snp <- item.acc
+    tre.mt <- tre.mt[perm, ]
   }
   
+  fit <- lm(tre.mt ~ groups.snp.f)
+  G <- tcrossprod(tre.mt)
+  X <- stats::model.matrix(fit) # Note contrast type
+  H <- tcrossprod(tcrossprod(X, solve(crossprod(X))), X)
+  numer <- crossprod(c(H), c(G))
+  trG <- sum(diag(G))
+  denom <- trG - numer
+  f.tilde <- as.numeric(numer/denom)  
+  R <- fit$residuals
+  df.e <- fit$df.residual
+  df.i <- nlevels(groups.snp.f) - 1
+  e <- eigen(cov(R)*(n-1)/df.e, symmetric = T, only.values = T)$values
+  lambda <- abs(e[abs(e) > 1e-12])
+  
+  item.acc <- 1e-14
+  pv.snp <- pcqf(q = f.tilde, lambda = lambda, df.i = df.i, df.e = df.e, acc = item.acc)
+  while (length(pv.snp) > 1) {
+    item.acc <- item.acc * 10
+    pv.snp <- pcqf(q = f.tilde, lambda = lambda, df.i = df.i, df.e = df.e, acc = item.acc)
+  }
+  if (pv.snp < item.acc) {
+    pv.snp <- item.acc
+  }
   return(data.frame(pv.snp))
 } 
+
 
 ##' Computes empirical P-value for a gene.
 ##' @title Compute empirical P-value
 ##' @param genotype.gene a data.frame of genotypes produced by \code{\link{read.bedix}}.
-##' @param tre.dist a interdistance matrix produced by \code{\link{hellingerDist}}.
+##' @param tre.mt a matrix of splicing ratios (samples x transcripts).
 ##' @param best.snp SNP with the smallest observed nominal P-value, computed by \code{\link{compute.nominal.pv}}.
 ##' @param min.pv.obs smallest observed nominal P-value.
-##' @param comp.ld should linkage disequilibrium estimates be computed (median R2). Default is TRUE.
+##' @param nb.perm.min the minimum number of permutations. Default is 100.
+##' @param nb.perm.max the maximum number of permutations. Default is 1000. 
 ##' @param min.nb.ext.scores the minimum number of permuted  nominal P-values lower than
 ##' the smallest observed nominal P-value to allow the computation to stop. Default is 100. 
-##' @param nb.perm.max the maximum number of permutations. Default is 1000. 
+##' @param comp.ld should linkage disequilibrium estimates be computed (median r2). Default is TRUE.
 ##' @param verbose Default is TRUE. Mainly for debugging.
 ##' @return a data.frame with columns:
 ##' \item{variants.cis}{the number of variants tested in cis.}
@@ -176,7 +218,7 @@ compute.nominal.pv <- function(geno.df, tre.dist, permute = FALSE){
 ##' \item{runtime}{approximated computation time per gene.}
 ##' @author Diego Garrido-Martín 
 ##' @keywords internal
-compute.empirical.pv <- function(genotype.gene, tre.dist, best.snp, min.pv.obs, min.nb.ext.scores = 100, nb.perm.max = 1000, comp.ld = TRUE, verbose = FALSE){
+compute.empirical.pv <- function(genotype.gene, tre.mt, best.snp, min.pv.obs, nb.perm.min = 100, nb.perm.max = 1000, min.nb.ext.scores = 100, comp.ld = TRUE, verbose = FALSE){
   if(comp.ld) {
     compute.ld <- function(df){
       M <- as.matrix(df)
@@ -192,9 +234,13 @@ compute.empirical.pv <- function(genotype.gene, tre.dist, best.snp, min.pv.obs, 
       }
       return(median(R, na.rm = T)) 
     }
-    ld <- compute.ld(genotype.gene[, labels(tre.dist)])
+    ld <- compute.ld(genotype.gene[, rownames(tre.mt)])
   }
+  variants.cis <- dim(genotype.gene)[1]
+  genotype.gene <- LD.filter(genotype.gene = genotype.gene, tre.mt = tre.mt, th = 1, tol = 0, tol.svqtl = NULL)             
+  genotype.gene$LD <- NULL
   if (verbose) {
+    message(sprintf("\tPASS not in perfect LD: %s", nrow(genotype.gene)))
     message ("\tAdaptative permutation scheme")
   }
   t0 <- Sys.time()
@@ -202,10 +248,10 @@ compute.empirical.pv <- function(genotype.gene, tre.dist, best.snp, min.pv.obs, 
   i <- 1 
   ext <- 0
   pv <- 1
-  while ( ext < min.nb.ext.scores & i <= nb.perm.max ) {
+  while ( i <= nb.perm.min || (ext < min.nb.ext.scores && i <= nb.perm.max) ) {
     # Note that i starts in 1. Thus "<=" instead of "<" in the while condition
     if (verbose & i%%100 == 0) message (sprintf("\t\tpermutation %s",i))
-    min.pv.perm <- min(dplyr::do(dplyr::group_by(genotype.gene, snpId),compute.nominal.pv(., tre.dist, permute = TRUE))$pv.snp)
+    min.pv.perm <- min(dplyr::do(dplyr::group_by(genotype.gene, snpId),compute.nominal.pv(., tre.mt, permute = TRUE, seed = i))$pv.snp)
     store.perm <- c(store.perm, min.pv.perm)
     ext <- sum(store.perm <= min.pv.obs)
     pv <- (ext + 1)/(i + 1)
@@ -218,7 +264,7 @@ compute.empirical.pv <- function(genotype.gene, tre.dist, best.snp, min.pv.obs, 
   pv.beta <- pbeta(min.pv.obs, shape1 = shape1, shape2 = shape2)  
   t1 <- Sys.time()
   t.run <- as.numeric(difftime(t1, t0, units = "mins"))
-  res.df <- data.frame(variants.cis = dim(genotype.gene)[1], LD = ld, best.snp = best.snp, best.nominal.pv = min.pv.obs, 
+  res.df <- data.frame(variants.cis = variants.cis, LD = ld, best.snp = best.snp, best.nominal.pv = min.pv.obs, 
                        shape1 = shape1, shape2 = shape2, nb.perm = length(store.perm), pv.emp.perm = pv, pv.emp.beta = pv.beta,
                        runtime = t.run) 
   return(res.df)
